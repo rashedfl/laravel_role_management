f518aaa4c95f5e1f97c081a90786a3d9e292ad37
rashed <rashedfl123@gmail.com>
2020-09-06 02:55:17 +0600

    modify login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header bg-dark text-light text-center">Role Management login</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
